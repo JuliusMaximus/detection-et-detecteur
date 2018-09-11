8eb51ed72c1f6d72c6c298ca666b26f92fb48a14
JuliusMaximus <contact@weckjulien.fr>
2018-09-11 22:10:33 -0100

    ajout du menu de navigation

---
+++ app/views/home/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="/css/index.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+	<title>Détection et détecteurs</title>
+</head>
+<body>
+	<header>
+		<?php include 'nav.php'; ?>
+	</header>
+	<section>
+		<article>
+			
+		</article>
+		<article>
+			
+		</article>
+	</section>
+	<!-- inclusion du footer -->
+	<?php include("footer.php"); ?>
+
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="/js/index.js"></script>
+</body>
+</html>
---
+++ app/views/home/nav.php
@@ -0,0 +1,43 @@
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="/">Détection et détecteurs</a>
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">La détection de métaux</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Déposer une annonce</a>
+        </li>
+      </ul>
+      <div class="btn-group dropdown">
+        <button type="button" class="btn btn-outline-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Mon compte <i class="far fa-user"></i>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+        <form class="px-4 py-3">
+          <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" class="form-control" id="email" placeholder="email@example.com">
+          </div>
+          <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input type="password" class="form-control" id="password" placeholder="*******">
+          </div>
+          <button type="submit" class="btn btn-warning">Connexion</button>
+        </form>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Pas encore inscrit?</a>
+        <a class="dropdown-item" href="#">Mot de passe oublié?</a>
+      </div>
+      </div> 
+    </div>
+  </div>
+</nav>
