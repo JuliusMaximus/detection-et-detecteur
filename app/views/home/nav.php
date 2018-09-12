62f3699e0432b55e3ebade84980c41302e0e5d29
JuliusMaximus <contact@weckjulien.fr>
2018-09-12 18:08:35 -0100

    ajout de fenêtres modales et des formulaires pour l'oubli de mot de passe et l'inscription

--- app/views/home/nav.php
+++ app/views/home/nav.php
@@ -1,9 +1,11 @@
 
 <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container">
+    <!-- menu burger -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <!-- navbar -->
     <a class="navbar-brand" href="/">Détection et détecteurs</a>
     <div class="collapse navbar-collapse" id="navbarToggler">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -17,12 +19,13 @@
           <a class="nav-link" href="#">Déposer une annonce</a>
         </li>
       </ul>
+      <!-- account connexion -->
       <div class="btn-group dropdown">
         <button type="button" class="btn btn-outline-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Mon compte <i class="far fa-user"></i>
         </button>
         <div class="dropdown-menu dropdown-menu-right">
-        <form class="px-4 py-3">
+        <form action="" method="post" class="px-4 py-3" role="form">
           <div class="form-group">
             <label for="email">E-mail</label>
             <input type="email" class="form-control" id="email" placeholder="email@example.com">
@@ -34,10 +37,70 @@
           <button type="submit" class="btn btn-warning">Connexion</button>
         </form>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Pas encore inscrit?</a>
-        <a class="dropdown-item" href="#">Mot de passe oublié?</a>
+        <button type="button" class="btn btn-primary m-1" data-toggle="modal" data-target="#inscription">
+          Pas encore inscrit?
+        </button>
+        <button type="button" class="btn btn-primary m-1" data-toggle="modal" data-target="#forgetpPassword">
+          Mot de passe oublié?
+        </button>
       </div>
       </div> 
     </div>
   </div>
 </nav>
+<!-- Modal inscription -->
+<div class="modal fade" id="inscription" tabindex="-1" role="dialog" aria-labelledby="Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="Title">Inscription</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post" role="form">
+          <div class="form-group">
+            <label for="emailInscription">Adresse e-mail</label>
+            <input type="email" class="form-control" id="emailInscription" aria-describedby="emailHelp" placeholder="exemple@email.com">
+            <small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais votre email avec quiconque.</small>
+          </div>
+          <div class="form-group">
+            <label for="passwordInscription">Mot de passe</label>
+            <input type="password" class="form-control" id="passwordInscription" placeholder="*******">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+        <button type="submit" class="btn btn-primary">Valider</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal forget password -->
+<div class="modal fade" id="forgetpPassword" tabindex="-1" role="dialog" aria-labelledby="Title" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="Title">Récupération de votre mot de passe</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post" role="form">
+          <div class="form-group">
+            <label for="emailRecup">Adresse e-mail</label>
+            <input type="email" class="form-control" id="emailrecup" aria-describedby="emailHelp" placeholder="exemple@email.com">
+            <small id="emailHelp" class="form-text text-muted">Entrez l'adresse email renseignée lors de votre incription.<br>Vous y recevrez un nouveau mot de passe.</small>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+        <button type="button" class="btn btn-primary">Valider</button>
+      </div>
+    </div>
+  </div>
+</div>
